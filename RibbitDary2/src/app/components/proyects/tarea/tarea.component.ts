b467f3b7ac528a3bfe5b1fba86af16be89835c00
gustavo-p3 <brandongustavo350@gmail.com>
2024-09-26 13:10:28 -0600

    Ya puedes ver y agregar ubicacion a las tareas

--- RibbitDary2/src/app/components/proyects/tarea/tarea.component.ts
+++ RibbitDary2/src/app/components/proyects/tarea/tarea.component.ts
@@ -1,10 +1,171 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProyectsService } from '../../../services/proyects.service';
+import { Tarea, Material } from '../../../models/Proyect';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-tarea',
   templateUrl: './tarea.component.html',
-  styleUrl: './tarea.component.css'
+  styleUrls: ['./tarea.component.css']
 })
-export class TareaComponent {
+export class TareaComponent implements OnInit {
+  private map: L.Map | undefined;
+  tarea: any = [];
+  tareaUbi: any = [];
+  colaUbi: any = [];
+  colaboradorTareas: any = [];
+  materiales: any = [];
+
+
+  constructor(
+    private proyectsService: ProyectsService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    const idP = this.route.snapshot.paramMap.get('idP');
+    const idU = this.route.snapshot.paramMap.get('idU');
+    const idT = this.route.snapshot.paramMap.get('idT');
+
+    if (idT && idP && idU) {
+      this.getTarea(idU, idP, idT);
+    }
+
+  }
+
+
+
+  getTarea(idU: string, idP: string, idT: string) {
+    this.proyectsService.getTarea(idU, idP, idT).subscribe(
+      resp => {
+        console.log(resp);
+        this.tarea = resp;
+        this.tareaUbi = resp;
+        this.getUsuario(this.tarea.idColaborador);
+        this.getMateriales(this.tarea.idT);  // Obtener y almacenar materiales de la tarea
+      },
+      err => console.error('Error al obtener tarea:', err)
+    )
+  }
+
+  getUsuario(idU: string) {
+    this.proyectsService.getUsuario(idU).subscribe(
+      resp => {
+        this.colaboradorTareas = resp;
+        this.colaUbi = resp;  // Almacenar datos de usuario en un objeto usando idU como clave
+        this.initMap();
+      },
+      err => console.error('Error al obtener usuario:', err)
+    );
+  }
+
+  getMateriales(idT: string) {
+    this.proyectsService.getMaterialesTarea(idT).subscribe(
+      resp => {
+        this.materiales = resp;  // Almacenar materiales en un objeto usando idT como clave
+      },
+      err => console.error('Error al obtener materiales:', err)
+    );
+  }
+
+
+  volver() {
+    const idP = this.route.snapshot.paramMap.get('idP');
+    const idU = this.route.snapshot.paramMap.get('idU');
+    this.router.navigate([`/tareas/${idU}/${idP}`]);
+  }
+
+
+
+  private initMap(): void {
+    const greenIcon = new L.Icon({
+      iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
+    });
+
+    const redIcon = new L.Icon({
+      iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
+    });
+    const yellowIcon = new L.Icon({
+      iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-yellow.png',
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
+    });
+
+    // Inicializar el mapa si aún no existe
+    if (!this.map) {
+      this.map = L.map('mi_mapa').setView([21.47268, -101.22288], 15);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(this.map);
+    }
+
+    // Mostrar la ubicación actual del usuario si se permite la geolocalización
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const userLocation: L.LatLngTuple = [
+            position.coords.latitude,
+            position.coords.longitude,
+          ];
+
+          if (this.map) {
+            this.map.setView(userLocation, 15);
+
+            // Añadir un marcador en la ubicación del usuario
+            L.marker(userLocation, { icon: redIcon })
+              .addTo(this.map)
+              .bindPopup("Estás aquí")
+              .openPopup();
+          }
+        },
+        () => {
+          alert('No se pudo obtener tu ubicación.');
+        }
+      );
+    } else {
+      alert('Geolocalización no soportada por tu navegador.');
+    }
+
+
+    if (this.tareaUbi.lat && this.tareaUbi.lng) {
+      const tareaLocation: L.LatLngTuple = [this.tareaUbi.lat, this.tareaUbi.lng];
+      L.marker(tareaLocation, { icon: yellowIcon })
+        .addTo(this.map!)
+        .bindPopup(`Ubicación de la tarea: ${this.tareaUbi.nomTarea}`)
+        .openPopup();
+    }
+
+    // Agregar marcadores para los colaboradores
+    if (this.map && this.colaboradorTareas.length > 0) {
+      this.colaboradorTareas.forEach((usuario: any) => {
+        const location: L.LatLngTuple = [usuario.lat, usuario.lng];
+        L.marker(location, { icon: greenIcon })
+          .addTo(this.map!)
+          .bindPopup(`${usuario.nombres} está aquí`);
+      });
+    }
+
+
+    this.map.on('click', function (e: L.LeafletMouseEvent) {
+      alert(`Has clickeado en la posición ${e.latlng.toString()}`);
+    });
+  }
+
 
 }
